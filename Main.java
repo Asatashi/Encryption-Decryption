2658833b5e87c3854d27410fca47280b3917c6a0
Asatashi <76907974+Asatashi@users.noreply.github.com>
2021-04-02 13:37:16 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,55 +1,65 @@
 package encryptdecrypt;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-     static void encryption(int keyArg,String word) {
-        StringBuilder result = new StringBuilder();
-        for (char character : word.toCharArray()) {
-            if (character != ' ') {
-                int originalAlphabetPosition = character - 'a';
-                int newAlphabetPosition = (originalAlphabetPosition + keyArg) % 94;
-                char newCharacter = (char) ('a' + newAlphabetPosition);
-                result.append(newCharacter);
-            } else {
-                result.append("%");
+        String mode = "enc";
+        String data = "";
+        int key = 0;
+        String inputPath = "";
+        String outputPath = "";
+
+        String argStr = Arrays.toString(args);
+        for (int i = 0; i < args.length; i += 2) {
+            if ("-mode".equals(args[i])) {
+                mode = args[i + 1];
+            }
+            if ("-key".equals(args[i])) {
+                key = Integer.parseInt(args[i + 1]);
+            }
+            if ("-data".equals(args[i])) {
+                data = args[i + 1];
+            }
+            if ("-in".equals(args[i]) && !argStr.contains("-data")) {
+                inputPath = args[i + 1];
+            }
+            if ("-out".equals(args[i])) {
+                outputPath = args[i + 1];
             }
         }
-         System.out.println(result);
-     }
 
-     static void decryption(int keyArg,String word) {
-         StringBuilder result = new StringBuilder();
-         for (char character : word.toCharArray()) {
-             if (character != '%') {
-                 int originalAlphabetPosition = character - 'a';
-                 int newAlphabetPosition = (originalAlphabetPosition - keyArg) % 94;
-                 char newCharacter = (char) ('a' + newAlphabetPosition);
-                 result.append(newCharacter);
-             } else {
-                 result.append(" ");
-             }
-         }
-         System.out.println(result);
-     }
-
-
-    public static void main(String[] args) {
-        int keyArg = 0;
-        boolean decrypt = false;
-        String word = "";
-        for (int i = 1; i < args.length; i+=2) {
-            if (args[i].equals("dec") && args[i - 1].equals("-mode"))
-                decrypt = true;
-            else if (args[i].matches("\\d+") && args[i - 1].equals("-key"))
-                keyArg = Integer.parseInt(args[i]);
-            else if (args[i].matches("^.*$") && args[i - 1].equals("-data"))
-                word = args[i];
+        if (!inputPath.isEmpty()) {
+            File inputFile = new File(inputPath);
+            Scanner scanner = new Scanner(inputFile);
+            data = scanner.nextLine();
+            scanner.close();
         }
-        if (!decrypt)
-            encryption(keyArg,word);
-        else
-            decryption(keyArg,word);
+
+        StringBuilder result = doEncDec(data, key, mode);
+
+        if (!outputPath.isEmpty()) {
+            File outputFile = new File(outputPath);
+            FileWriter writer = new FileWriter(outputFile);
+            writer.write(result.toString());
+            writer.close();
+        } else {
+            System.out.print(result);
+        }
+    }
+
+    public static StringBuilder doEncDec(String data, int key, String mode) {
+        StringBuilder str = new StringBuilder();
+        if ("dec".equals(mode)) {
+            key = -key;
+        }
+        for (int i = 0; i < data.length(); i++) {
+            str.append(Character.toChars(data.charAt(i) + key));
+        }
+        return str;
     }
 }
